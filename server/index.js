ae1be13e8246a6e310480ca85d0d39105370dc8f
Amateur Audio Dude <168192910+AmateurAudioDude@users.noreply.github.com>
2025-04-20 21:24:29 +1000

    exclude localhost from 'dangerous command' warning

--- server/index.js
+++ server/index.js
@@ -421,10 +421,12 @@ wss.on('connection', (ws, request) => {
     ws.on('message', (message) => {
         const command = helpers.antispamProtection(message, clientIp, ws, userCommands, lastWarn, userCommandHistory, '18', 'text');
 
-        if (((command.startsWith('X') || command.startsWith('Y')) && !request.session.isAdminAuthenticated) || 
-           ((command.startsWith('F') || command.startsWith('W')) && serverConfig.bwSwitch === false)) {
-            logWarn(`User \x1b[90m${clientIp}\x1b[0m attempted to send a potentially dangerous command. You may consider blocking this user.`);
-            return;
+        if (!clientIp.includes("127.0.0.1")) {
+            if (((command.startsWith('X') || command.startsWith('Y')) && !request.session.isAdminAuthenticated) || 
+               ((command.startsWith('F') || command.startsWith('W')) && serverConfig.bwSwitch === false)) {
+                logWarn(`User \x1b[90m${clientIp}\x1b[0m attempted to send a potentially dangerous command: ${command.slice(0, 64)}.`);
+                return;
+            }
         }
 
         if (command.includes("\'")) {
