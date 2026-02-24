1edfc832d98c28edd0845b3cc3018b5d192751cc
Marek Farkaš <noobish@noobish.eu>
2026-02-24 15:21:15 +0100

    ver bump

--- web/js/ver.js
+++ web/js/ver.js
@@ -1,2 +1,2 @@
-const versionDate = new Date('Feb 24, 2026 01:00:00');
-const currentVersion = `v1.4.0 [${versionDate.getDate()}/${versionDate.getMonth() + 1}/${versionDate.getFullYear()}]`;
+const versionDate = new Date('Feb 24, 2026 15:00:00');
+const currentVersion = `v1.4.0a [${versionDate.getDate()}/${versionDate.getMonth() + 1}/${versionDate.getFullYear()}]`;
